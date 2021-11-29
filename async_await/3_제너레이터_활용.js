d12aa12b44f0c1eaa0fcb7019daef40f631df327
naerim <naerim1119@gmail.com>
2021-11-29 21:34:58 +0900

    제너레이터 활용

--- async_await/3_제너레이터_활용.js
+++ async_await/3_제너레이터_활용.js
@@ -1,12 +1,13 @@
-
+// 제너레이터를 사용해서 비동기처리를 동기 처리처럼 동작하도록 구현한 것
+// -> 간단하고 가독성 좋게 비동기 처리를 동기 처리처럼 동작하도록 하는 async await 도입!
 // node-fetch : node환경에서 window.fetch 함수를 사용하기 위한 패키지
 const fetch = require('node-fetch');
 
 // 제너레이터 실행기
-const async = generatorFunc => {
-    const generator = generatorFunc();
+const async = generatorFunc => {s
+    const generator = generatorFunc(ㄴ);s
     const onResolved = arg => {
-        const result = generator.next(arg);
+        const result = generator.next(arg);ㄴ
         return result.done
         ? result.value
         : result.value.then(res => onResolved(res));
